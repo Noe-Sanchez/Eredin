e287e54a5e4d1c83c35604c70d002fbdaaa5759e
Noe-Sanchez <danfoot4567@gmail.com>
2025-06-26 04:22:00 -0600

    Working UART TX comms, RX needs tweaks

--- eredin_core/src/main.rs
+++ eredin_core/src/main.rs
@@ -3,72 +3,137 @@
 
 use panic_halt as _;
 use cortex_m_rt::entry;
-use stm32h7::stm32h755cm7;
+use stm32h7::stm32h755cm7 as pac;
+use core::fmt::Write;
+
+struct Uart {
+    usart: pac::USART3,
+}
+
+impl Uart {
+    fn new(usart: pac::USART3) -> Self {
+        Self { usart }
+    }
+    
+    fn init(&mut self, rcc: &pac::RCC, gpiod: &pac::GPIOD) {
+        // Enable clocks
+        // Enable GPIOD clock
+        rcc.ahb4enr().modify(|_, w| w.gpioden().set_bit());
+        
+        // Enable USART3 clock  
+        rcc.apb1lenr().modify(|_, w| w.usart3en().set_bit());
+        
+        // Configure PD8 (TX) and PD9 (RX) as alternate function
+        // PD8 - USART3_TX (AF7)
+        gpiod.moder().modify(|_, w| w.moder8().alternate());
+        gpiod.afrh().modify(|_, w| w.afr8().af7());
+        gpiod.ospeedr().modify(|_, w| w.ospeedr8().very_high_speed());
+        
+        // PD9 - USART3_RX (AF7) 
+        gpiod.moder().modify(|_, w| w.moder9().alternate());
+        gpiod.afrh().modify(|_, w| w.afr9().af7());
+        gpiod.ospeedr().modify(|_, w| w.ospeedr9().very_high_speed());
+        
+        // Configure USART3
+        // Disable USART first
+        self.usart.cr1().modify(|_, w| w.ue().clear_bit());
+        
+        // Set baud rate (assuming 64MHz APB1 clock, target 115200 baud)
+        // BRR = fCK / baud_rate = 64000000 / 115200 ≈ 555 (0x22B)
+        self.usart.brr().write(|w| unsafe { w.bits(555) });
+        
+        // Configure: 8 data bits, no parity, 1 stop bit
+        self.usart.cr1().modify(|_, w| {
+            w.m0().clear_bit()      // 8 data bits
+             .pce().clear_bit()     // No parity
+             .te().set_bit()        // Transmitter enable
+             .re().set_bit()        // Receiver enable
+        });
+        
+        self.usart.cr2().modify(|_, w| unsafe {
+            w.stop().bits(0b00)     // 1 stop bit
+        });
+        
+        // Enable USART
+        self.usart.cr1().modify(|_, w| w.ue().set_bit());
+    }
+    
+    fn write_byte(&mut self, byte: u8) {
+        // Wait for transmit data register to be empty
+        while self.usart.isr().read().txe().bit_is_clear() {}
+        
+        // Write data
+        self.usart.tdr().write(|w| unsafe { w.tdr().bits(byte as u16) });
+    }
+    
+    fn read_byte(&mut self) -> Option<u8> {
+        if self.usart.isr().read().rxne().bit_is_set() {
+            Some(self.usart.rdr().read().rdr().bits() as u8)
+        } else {
+            None
+        }
+    }
+    
+    fn write_str(&mut self, s: &str) {
+        for byte in s.bytes() {
+            self.write_byte(byte);
+        }
+    }
+}
+
+impl Write for Uart {
+    fn write_str(&mut self, s: &str) -> core::fmt::Result {
+        self.write_str(s);
+        Ok(())
+    }
+}
+
+fn setup_system_clock(rcc: &pac::RCC) {
+    // Basic clock setup - using HSI (64MHz) for simplicity
+    // In production, you'd want to configure PLL for higher speeds
+    
+    // Ensure HSI is on
+    rcc.cr().modify(|_, w| w.hsion().set_bit());
+    while rcc.cr().read().hsirdy().bit_is_clear() {}
+    
+    // Select HSI as system clock
+    rcc.cfgr().modify(|_, w| w.sw().hsi());
+    while !rcc.cfgr().read().sws().is_hsi() {}
+}
 
 #[entry]
 fn main() -> ! {
-    // Get peripherals
-    let p = stm32h755cm7::Peripherals::take().unwrap();
+    let dp = pac::Peripherals::take().unwrap();
+    let _cp = cortex_m::Peripherals::take().unwrap();
     
-    // Enable GPIOB clock (LED is on PB0 for STM32H755-Nucleo)
-    p.RCC.ahb4enr().modify(|_, w| w.gpioben().set_bit());
-
-    // Enable GPIOE clock
-    p.RCC.ahb4enr().modify(|_, w| w.gpioeen().set_bit());
+    // Setup system clock
+    setup_system_clock(&dp.RCC);
     
-    // Configure PB0 as output
-    p.GPIOB.moder().modify(|_, w| w.moder0().output());
-    p.GPIOB.otyper().modify(|_, w| w.ot0().push_pull());
-    p.GPIOB.ospeedr().modify(|_, w| w.ospeedr0().low_speed());
-    p.GPIOB.pupdr().modify(|_, w| w.pupdr0().floating());
-
-    // Configure PB14 as output
-    p.GPIOB.moder().modify(|_, w| w.moder14().output());
-    p.GPIOB.otyper().modify(|_, w| w.ot14().push_pull());
-    p.GPIOB.ospeedr().modify(|_, w| w.ospeedr14().low_speed());
-    p.GPIOB.pupdr().modify(|_, w| w.pupdr14().floating());
-
-    // Configure PE1 as output
-    p.GPIOE.moder().modify(|_, w| w.moder1().output());
-    p.GPIOE.otyper().modify(|_, w| w.ot1().push_pull());
-    p.GPIOE.ospeedr().modify(|_, w| w.ospeedr1().low_speed());
-    p.GPIOE.pupdr().modify(|_, w| w.pupdr1().floating());
-
-    // Turn LED on
-    p.GPIOB.bsrr().write(|w| w.bs0().set_bit());
-
-    // Turn PB14 on
-    p.GPIOB.bsrr().write(|w| w.bs14().set_bit());
-
-    // Turn PE1 on
-    p.GPIOE.bsrr().write(|w| w.bs1().set_bit());
+    // Initialize UART
+    let mut uart = Uart::new(dp.USART3);
+    uart.init(&dp.RCC, &dp.GPIOD);
+    
+    // Send initial message
+    uart.write_str("STM32H755 UART Communication Started!\r\n");
+   
+    
+    let mut counter = 0u32;
     
     loop {
-        // Turn LED on
-        p.GPIOB.bsrr().write(|w| w.bs0().set_bit());
-        p.GPIOB.bsrr().write(|w| w.br14().set_bit());
-        p.GPIOE.bsrr().write(|w| w.br1().set_bit());
+        
+        // Send periodic messages
+        write!(uart, "Counter: {}\r\n", counter).ok();
+        counter = counter.wrapping_add(1);
+        
+        // Check for received data and echo it back
+        if let Some(received_byte) = uart.read_byte() {
+            uart.write_str("Received: ");
+            uart.write_byte(received_byte);
+            uart.write_str("\r\n");
+        }
         
         // Simple delay
-        for _ in 0..25_000 {
-            cortex_m::asm::nop();
-        }
-
-        p.GPIOB.bsrr().write(|w| w.br0().set_bit());
-        p.GPIOB.bsrr().write(|w| w.bs14().set_bit());
-        p.GPIOE.bsrr().write(|w| w.br1().set_bit());
-        
-        // Simple delay
-        for _ in 0..25_000 {
-            cortex_m::asm::nop();
-        }
-
-        p.GPIOB.bsrr().write(|w| w.br0().set_bit());
-        p.GPIOB.bsrr().write(|w| w.br14().set_bit());
-        p.GPIOE.bsrr().write(|w| w.bs1().set_bit());
-
-        // Simple delay
-        for _ in 0..25_000 {
+        for _ in 0..1_000_000 {
             cortex_m::asm::nop();
         }
     }
