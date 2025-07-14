e7f58588093d9fc83496bb70d52a642d14813a74
Noe-Sanchez <danfoot4567@gmail.com>
2025-07-13 23:12:00 -0600

    Hardware nvic interrupt for usart3 in rtic, needs validation before sitl

--- eredin_core/src/main.rs
+++ eredin_core/src/main.rs
@@ -2,17 +2,6 @@
 #![no_main]
 #![no_std]
 
-//use cortex_m_rt::entry;
-
-//use stm32h7xx_hal::{pac, prelude::*};
-
-//use core::fmt::Write;
-
-//use stm32h7xx_hal::nb::block;
-
-//#[entry]
-//fn main() -> ! {
-
 use panic_halt as _;
 use rtic::app;
 use rtic_monotonics::systick::prelude::*;
@@ -42,29 +31,21 @@ mod app {
       led_r: PA0<Output<PushPull>>,
       led_g: PA1<Output<PushPull>>,
       led_b: PA2<Output<PushPull>>,
-      //tx:    Tx<USART3>,
-      //rx:    Rx<USART3>,
-      //tx:    PB10<Alternate<7>>,
-      //rx:    PB11<Alternate<7>>,
       serial: stm32h7xx_hal::serial::Serial<USART3>,
-      //serial: stm32h7xx_hal::serial::Serial<pac::USART3, (PB10<Alternate>, PB11<Alternate>)>,
     }
 
     #[local]
-    struct Local {}
+    struct Local {
+      read_data: [u8; 64],
+      idx: u8,
+    }
 
     #[init]
-    //fn init(con: init::Context) -> (Shared, Local, init::Monotonics){
     fn init(con: init::Context) -> (Shared, Local){
     
-      //let dp = pac::Peripherals::take().unwrap();
-      //let cp = cortex_m::Peripherals::take().unwrap();
-      //let dp = con.device::Peripherals::take().unwrap();
-      //let cp = cortex_m::Peripherals::take().unwrap();
       let dp = con.device; 
       let cp = con.core;
 
-      //Mono::start(con.core.SYST, 400_000_000);
       Mono::start(cp.SYST, 400_000_000);
 
       // Constrain and Freeze power
@@ -73,62 +54,55 @@ mod app {
 
       // Constrain and Freeze clock
       let rcc = dp.RCC.constrain();
-      //let ccdr = rcc.sys_ck(160.MHz()).freeze(pwrcfg, &dp.SYSCFG);
       let ccdr = rcc.sys_ck(400.MHz()).freeze(pwrcfg, &dp.SYSCFG);
 
-      // Acquire the GPIOC peripheral. This also enables the clock for
-      // GPIOC in the RCC register.
+      // Enable GPIOA/B clocks
       let gpiob = dp.GPIOB.split(ccdr.peripheral.GPIOB);
       let gpioa = dp.GPIOA.split(ccdr.peripheral.GPIOA);
 
+      // Pins for LEDs
       let led_r = gpioa.pa0.into_push_pull_output();
       let led_g = gpioa.pa1.into_push_pull_output();
       let led_b = gpioa.pa2.into_push_pull_output();
 
+      // Pins for USART3
       let tx = gpiob.pb10.into_alternate();
       let rx = gpiob.pb11.into_alternate();
 
-
-      //let mut delay = cp.SYST.delay(ccdr.clocks);
-
       // Configure the serial peripheral.
-      let serial = dp
+      let mut serial = dp
           .USART3
           .serial((tx, rx), 115_200.bps(), ccdr.peripheral.USART3, &ccdr.clocks)
           .unwrap();
 
+      serial.listen(stm32h7xx_hal::serial::Event::Rxne);
 
-      //let (mut tx, mut _rx) = serial.split();
-      
-      // Write a message to the serial port
-      //tx.write(b'H').unwrap();
+      // Greet before spinning
+      writeln!(serial, "Eredin> Starting Scheduler...\r").unwrap();
 
-      
-
-      // Scheduling
-
-      /*task_blink_led1::spawn_after(200.millis()).unwrap();
-      task_blink_led2::spawn_after(500.millis()).unwrap();
-      task_blink_led3::spawn_after(1000.millis()).unwrap();*/
+      // Schedule software tasks
       task_blink_led1::spawn().ok();
       task_blink_led2::spawn().ok();
       task_blink_led3::spawn().ok();
       task_print::spawn().ok();
+      task_print2::spawn().ok();
+      let read_data: [u8; 64] = [0; 64]; 
+      let idx: u8 = 0;
 
-
+      // Resources for tasks
       (
         Shared {
           led_r,
           led_g,
           led_b,
           serial,
-          //serial: serial,
         },
-        Local {},
-        //init::Monotonics(rr),
+        Local {
+          read_data,
+          idx,
+        },
       )
   }
-
 
   #[task(shared = [led_r])]
   async fn task_blink_led1(con: task_blink_led1::Context) {
@@ -168,19 +142,48 @@ mod app {
     let mut serial_if = con.shared.serial; 
     loop {
       serial_if.lock(|serial| {
-          // Write a message to the serial port
-          /*serial.write(b'H').unwrap();
-          serial.write(b'e').unwrap();
-          serial.write(b'l').unwrap();
-          serial.write(b'l').unwrap();
-          serial.write(b'o').unwrap();
-          serial.write(b'\r').unwrap();
-          serial.write(b'\n').unwrap();*/
-          writeln!(serial, "Hello from RTIC!\r").unwrap();
+          writeln!(serial, "Task1> Hello from RTIC Task1!\r").unwrap();
       });
 
       Mono::delay(1000.millis()).await;
     }
+  }
+  #[task(shared = [serial])]
+  async fn task_print2(con: task_print2::Context) {
+    let mut serial_if = con.shared.serial; 
+    loop {
+      serial_if.lock(|serial| {
+          writeln!(serial, "Task2> Hello from RTIC Task2!\r").unwrap();
+      });
+
+      Mono::delay(500.millis()).await;
+    }
+  }
+  #[task(binds = USART3, shared = [serial], local = [read_data, idx])]
+  fn task_receive(con: task_receive::Context) {
+    let mut serial_if = con.shared.serial; 
+
+    let byte = serial_if.lock(|serial| {
+      serial.read()
+    });
+    let read_data = con.local.read_data;
+    let idx = con.local.idx; 
+    if let Ok(byte) = byte {
+      read_data[*idx as usize] = byte;
+      *idx += 1;
+    }
+    if *idx >= read_data.len() as u8 || read_data[*idx as usize - 1] == 13 {
+      *idx = 0; // Reset index if it exceeds buffer size
+      serial_if.lock(|serial| {
+        let read_str = core::str::from_utf8(&read_data[..]).unwrap_or("Invalid UTF-8"); 
+        writeln!(serial, "TaskReceive> Read data: {}", read_str).unwrap();
+      });
+      // Clear the buffer
+      for i in 0..read_data.len() {
+        read_data[i] = 0; // Clear the buffer
+      }
+    }
+
   }
 
 
